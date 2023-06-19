45d2e0f2daa6f085c3eb059a3a7ecdf7c6fc7ea5
Hanyu Tatsuya <hanyu@bml.co.jp>
2023-06-19 10:11:56 +0900

    Commit 20230619-101148

--- public_html/index.php
+++ public_html/index.php
@@ -26,60 +26,6 @@
 //	printf("<DIV align=right><A href=php/phpinfo.php>phpinfo</A></DIV>\n");
 	printf("<HR>\n");
 
-	try {
-		$conn = GetDBConn();
-		if ($conn) {
-			$sql = "";
-			$sql = $sql . "select knsgrp,kmkcd";
-			$sql = $sql . " from kmkmst";
-			$sql = $sql . " where knsgrp = '0054'";
-			$sql = $sql . " order by kmkcd for read only";
-	
-		//	print $sql;
-	
-			$i = 0;
-			foreach ($conn->query($sql) as $row) {
-				printf("%s　%s<BR>\n",$row['KNSGRP'],$row['KMKCD']);
-				$i += 1;
-			}
-
-//			foreach ($conn->query($sql) as $row) {
-//				$Inform[$i][0] = $row['knsgrp'];
-//				$Inform[$i][1] = $row['kmkcd'];
-//				$i += 1;
-//			}
-	
-//			if (count($Inform) != 0) {
-//				printf("<center>\n");		
-//				printf("<div>お知らせ</div>\n");
-//				printf("<div>\n");
-
-//				for ($i=0;$i<count($Inform);$i++) {
-//						printf("%s　%s<BR>\n",date("Y-m-d",$Inform[$i][0]),$Inform[$i][1]);
-//					printf("%s　%s<BR>\n",$Inform[$i][0],$Inform[$i][1]);
-//				}
-	
-//				printf("</div>\n");
-//				printf("</center>\n");		
-//			}
-	
-//			odbc_close($conn);
-		} else {
-			printf("Connection failed\n");
-		}
-	}
-	catch (ODBCException $PDOex) {
-		LaboLogOut(sprintf("SQLMSG  [%s]",$PDOex->getMessage()));
-		LaboLogOut(sprintf("SQLCODE  [%s]",$PDOex->getCode()));
-	}
-	catch (ODBCException $ex) {
-		LaboLogOut(sprintf("ERRMSG  [%s]",$ex->getMessage()));
-		LaboLogOut(sprintf("ERRCODE  [%s]",$ex->getCode()));
-	}
-
-	$conn = null;
-
-
 //	printf("<HR>\n");
 	printf("<DIV align=right><A href=php/phpinfo.php>phpinfo</A></DIV>\n");
 
