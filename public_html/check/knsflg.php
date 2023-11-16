b10a3cd0dbeb519778d9a80f7acc6909b1f11b81
Hanyu Tatsuya <hanyu@bml.co.jp>
2023-11-16 11:32:20 +0900

    Commit 20231116-113211

--- public_html/check/knsflg.php
+++ public_html/check/knsflg.php
@@ -66,33 +66,110 @@ if (strlen($udate) != 0) {
 	if ($conn) {
 
 		try {
+			$cnt = 0;
 			printf("<table align=\"center\" bgcolor = white border>\n");
 				printf("<tr>\n");
+					printf("<th nowarp>　　</th>\n");
 					printf("<th nowarp>BML受付年月日</th>\n");
 					printf("<th nowarp>依頼書No</th>\n");
 					printf("<th nowarp>項目CD</th>\n");
+					printf("<th nowrap>項目名</th>\n");
+					printf("<th nowrap>検査回数</th>\n");
+					printf("<th nowrap>結果状況CD</th>\n");
+					printf("<th nowrap>検査F</th>\n");
+					printf("<th nowrap>照会結果</th>\n");
 				printf("</tr>\n");
 	
 				$sql = "";
-				$sql = $sql . "select distinct utkymd,irino,kmkcd ";
-				$sql = $sql . "from kekka ";
-				$sql = $sql . "where utkymd in (SELECT DISTINCT KJNSRIYMD FROM SYSKNRMST) ";
-				$sql = $sql . "and knsflg = 'M' ";
-				$sql = $sql . "and kkajkcd = '3' ";
-				$sql = $sql . "order by utkymd,irino,kmkcd ";
+				$sql = $sql . "select distinct ka.utkymd,ka.irino,ka.kmkcd,km.kmkrs,ka.knssu,ka.kkajkcd,ka.knsflg,ka.skkka ";
+				$sql = $sql . "from kekka ka,kmkmst km ";
+				$sql = $sql . "where ka.utkymd = '$udate' ";
+				$sql = $sql . "and ka.knsflg = 'M' ";
+				$sql = $sql . "and ka.kkajkcd = '3' ";
+				$sql = $sql . "and km.knsgrp = ka.knsgrp ";
+				$sql = $sql . "and km.kmkcd = ka.kmkcd ";
+				$sql = $sql . "order by ka.utkymd,ka.irino,ka.kmkcd ";
 				$sql = $sql . "for read only with ur ";
 	
 				foreach ($conn->query($sql,PDO::FETCH_NUM) as $row) {
 					printf("<tr>\n");
+						printf("<td nowrap> </td>\n");
 						printf("<td nowrap>%s</td>\n",SjToUtfConv($row[0]));
 						printf("<td nowrap>%s</td>\n",SjToUtfConv($row[1]));
 						printf("<td nowrap>%s</td>\n",SjToUtfConv($row[2]));
+						printf("<td nowrap>%s</td>\n",SjToUtfConv($row[3]));
+						printf("<td nowrap>%s</td>\n",SjToUtfConv($row[4]));
+						printf("<td nowrap>%s</td>\n",SjToUtfConv($row[5]));
+						printf("<td nowrap>%s</td>\n",SjToUtfConv($row[6]));
+						printf("<td nowrap>%s</td>\n",SjToUtfConv($row[7]));
 					printf("</tr>\n");
+					$cnt = $cnt + 1;
 				}
 	
 			printf("</table>\n");
 	
 			printf("<BR clear=all>\n");
+			
+			if ($cnt > 0) {
+				printf("<FORM ACTION=\"knsflgupd.php\" METHOD=POST>\n");
+				printf("<table align=\"center\" bgcolor = white border>\n");
+					printf("<tr>\n");
+						printf("<th nowarp>CLR</th>\n");
+						printf("<th nowarp>BML受付年月日</th>\n");
+						printf("<th nowarp>依頼書No</th>\n");
+						printf("<th nowarp>項目CD</th>\n");
+						printf("<th nowrap>項目名</th>\n");
+						printf("<th nowrap>検査回数</th>\n");
+						printf("<th nowrap>結果状況CD</th>\n");
+						printf("<th nowrap>検査F</th>\n");
+						printf("<th nowrap>照会結果</th>\n");
+					printf("</tr>\n");
+		
+					$sql = "";
+					$sql = $sql . "select distinct ka.utkymd,ka.irino,ka.kmkcd,km.kmkrs,ka.knssu,ka.kkajkcd,ka.knsflg,ka.skkka ";
+					$sql = $sql . "from kekka ka,kmkmst km ";
+					$sql = $sql . "where ka.utkymd = '$udate' ";
+					$sql = $sql . "and km.knsgrp = ka.knsgrp ";
+					$sql = $sql . "and km.kmkcd = ka.kmkcd ";
+					$sql = $sql . "and exists (select * from kekka k1 ";
+					$sql = $sql . "where k1.utkymd = ka.utkymd ";
+					$sql = $sql . "and k1.irino = ka.irino ";
+					$sql = $sql . "and k1.knsgrp = ka.knsgrp ";
+					$sql = $sql . "and k1.kmkcd = ka.kmkcd ";
+					$sql = $sql . "and k1.knsflg = 'M' ";
+					$sql = $sql . "and k1.kkajkcd = '3') ";
+					$sql = $sql . "order by ka.utkymd,ka.irino,ka.kmkcd ";
+					$sql = $sql . "for read only with ur ";
+		
+					foreach ($conn->query($sql,PDO::FETCH_NUM) as $row) {
+						printf("<tr>\n");
+							printf("<td nowrap>\n");
+							if ($row[5] == '3' && $row[6] == 'M') {
+								printf("<INPUT TYPE=RADIO NAME=KUBUN VALUE=\"0\">\n");
+							}
+							printf("</td>\n");
+							printf("<td nowrap>%s</td>\n",SjToUtfConv($row[0]));
+							printf("<td nowrap>%s</td>\n",SjToUtfConv($row[1]));
+							printf("<td nowrap>%s</td>\n",SjToUtfConv($row[2]));
+							printf("<td nowrap>%s</td>\n",SjToUtfConv($row[3]));
+							printf("<td nowrap>%s</td>\n",SjToUtfConv($row[4]));
+							printf("<td nowrap>%s</td>\n",SjToUtfConv($row[5]));
+							printf("<td nowrap>%s</td>\n",SjToUtfConv($row[6]));
+							printf("<td nowrap>%s</td>\n",SjToUtfConv($row[7]));
+						printf("</tr>\n");
+						$cnt = $cnt + 1;
+					}
+		
+				printf("</table>\n");
+				printf("<BR>\n");
+				printf("<center>\n");
+				printf("<BUTTON TYPE=submit name=upd value=\"upd\">更新\n");
+				printf("</BUTTON>\n");
+				printf("</center>\n");
+				printf("</FORM>\n");
+		
+				printf("<BR clear=all>\n");
+			}
 		}
 		catch (Exception $ex) {
 			GetDBErrMsg($ex);
